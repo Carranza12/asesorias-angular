32a57929eadc12ce6c6e48ef8a45599815e607a8
carranza12 <franckgta3@gmail.com>
2024-08-31 10:27:00 -0600

    logica inicial

--- src/app/products/form/form.component.ts
+++ src/app/products/form/form.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PreviewComponent } from '../preview/preview.component';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, PreviewComponent, ListComponent],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  name = new FormControl('', Validators.required);
+  price = new FormControl('', Validators.required);
 
+  ngOnInit() {}
 }
--- src/app/products/preview/preview.component.ts
+++ src/app/products/preview/preview.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import {
+  Component,
+  Input,
+  input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './preview.component.html',
-  styleUrl: './preview.component.css'
+  styleUrl: './preview.component.css',
 })
-export class PreviewComponent {
+export class PreviewComponent implements OnChanges {
+  @Input() name!: any;
+  @Input() price!: any;
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['name']) {
+      this.name = changes['name'].currentValue;
+    }
+    if (changes['price']) {
+      this.price = changes['price'].currentValue;
+    }
+  }
 }
